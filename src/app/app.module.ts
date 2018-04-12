bcbab2d5e1f679df7e1135fbdd46ccd41f64af01
Alejandro Anzures <alejandreyes36@gmail.com>
2018-04-11 22:44:33 -0500

    Subir base del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LoginComponent } from './componentes/login/login.component';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './componentes/usuario/usuario.component';
 
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'usuario', component: UsuarioComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
